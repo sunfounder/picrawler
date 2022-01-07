5e65e40b4cc548583f408952ff4250fefb24fe1f
dino <845864704@qq.com>
2022-01-07 13:58:03 +0800

    Optimize keyboard control

--- examples/record_video.py
+++ examples/record_video.py
@@ -1,26 +1,64 @@
-from os import pardir
-from time import sleep
+from time import sleep,strftime,localtime
 from vilib import Vilib
+import readchar 
+
+manual = '''
+Press keys on keyboard to control recording:
+    Q: record/pause/continue
+    E: stop
+    ESC: Quit
+'''
+
+def print_overwrite(msg,  end='', flush=True):
+    print('\r\033[2K', end='',flush=True)
+    print(msg, end=end, flush=True)
 
 def main():
-    Vilib.camera_start()
-    Vilib.display()
+    rec_flag = 'stop' # start,pause,stop
+    vname = None
+    Vilib.rec_video_set["path"] = "/home/pi/Videos/" # set path
 
-    Vilib.rec_video_set["path"] = "/home/pi/video/test/"
-    vname = "vtest"
-    Vilib.rec_video_run(vname)
-    print('start rec ...')
+    Vilib.camera_start(vflip=False,hflip=False) 
+    Vilib.display(local=True,web=True)
+    sleep(0.8)  # wait for startup
+
+    print(manual)
     while True:
-        if input() == 'q':
-            Vilib.rec_video_start()
-            print('continue')
-        if input() == 'w':
-            Vilib.rec_video_pause()
-            print('pause')                                                       
-        if input() == 'e':
+        # read keyboard
+        key = readchar.readkey()
+        key = key.lower()
+        # start,pause
+        if key == 'q':
+            key = None
+            if rec_flag == 'stop':            
+                rec_flag = 'start'
+                # set name
+                vname = strftime("%Y-%m-%d-%H.%M.%S", localtime())
+                Vilib.rec_video_set["name"] = vname
+                # start record
+                Vilib.rec_video_run()
+                print_overwrite('rec start ...')
+            elif rec_flag == 'start':
+                rec_flag = 'pause'
+                Vilib.rec_video_pause()
+                print_overwrite('pause')
+            elif rec_flag == 'pause':
+                rec_flag = 'start'
+                Vilib.rec_video_start()
+                print_overwrite('continue')
+        # stop       
+        elif key == 'e' and rec_flag != 'stop':
+            key = None
+            rec_flag = 'stop'
             Vilib.rec_video_stop()
-            print('stop')
-            print("The video saved as",Vilib.rec_video_set["path"],vname)
+            print_overwrite("The video saved as %s%s.avi"%(Vilib.rec_video_set["path"],vname),end='\n')  
+        # quit
+        elif key == readchar.key.CTRL_C or key in readchar.key.ESCAPE_SEQUENCES:
+            Vilib.camera_close()
+            print('\nquit')
+            break 
+
+        sleep(0.1)
 
 if __name__ == "__main__":
     main()
