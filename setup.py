c2536352ba5cb613ab232daadf3229ad638e7e83
dino <845864704@qq.com>
2022-01-07 14:03:41 +0800

    add readchar library

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['robot_hat'],
+    install_requires=['robot_hat', 'readchar'],
  
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
