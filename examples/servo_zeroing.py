a919e1fdf59f312678935e4af346b877a948e305
dino <845864704@qq.com>
2022-01-28 10:47:46 +0800

    add reset_mcu

--- examples/servo_zeroing.py
+++ examples/servo_zeroing.py
@@ -1,7 +1,10 @@
-from robot_hat import PWM
-from robot_hat import Servo
+from robot_hat import PWM,Servo
+from robot_hat.utils import reset_mcu
+from time import sleep
+
+reset_mcu()
+sleep(0.2)
 
 zeroing_pin = Servo(PWM('P11'))
-
 zeroing_pin.angle(0) 
 
