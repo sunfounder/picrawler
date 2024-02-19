142902aeac456fed5bc0dcace8cb0b1775a65aab
Lovmong <1967471283@qq.com>
2024-02-19 14:39:03 +0800

    add moving to 10 deg firstly

--- examples/servo_zeroing.py
+++ examples/servo_zeroing.py
@@ -5,6 +5,15 @@ from time import sleep
 reset_mcu()
 sleep(0.2)
 
-zeroing_pin = Servo(PWM('P11'))
-zeroing_pin.angle(0) 
+# zeroing_pin = Servo(PWM('P11'))
+# zeroing_pin.angle(0) 
 
+if __name__ == '__main__':
+    for i in range(12):
+        print(f"Servo {i} set to zero")
+        Servo(PWM(i)).angle(10)
+        sleep(0.1)
+        Servo(PWM(i)).angle(0)
+        sleep(0.1)
+    while True:
+        sleep(1)
