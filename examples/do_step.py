4b8a17ffca5f3b3b407f620e0155a5a3ddf05b01
Cavon Lee <cavonxx@gmail.com>
2024-05-16 10:12:24 +0800

    add loop back

--- examples/do_step.py
+++ examples/do_step.py
@@ -9,15 +9,15 @@ new_step=[[45, 45, -75], [45, 0, -75], [45, 0, -30], [45, 45, -75]]
 stand_step = crawler.move_list['stand'][0]
 
 def main():  
-    
-    speed = 80
-        
-    print(f"stand step: {stand_step}")
-    crawler.do_step(stand_step, speed)
-    sleep(3)
-    print(f"new step: {new_step}")
-    crawler.do_step(new_step,speed)
-    sleep(3)
+    while True:
+        speed = 80
+            
+        print(f"stand step: {stand_step}")
+        crawler.do_step(stand_step, speed)
+        sleep(3)
+        print(f"new step: {new_step}")
+        crawler.do_step(new_step,speed)
+        sleep(3)
             
  
 if __name__ == "__main__":
