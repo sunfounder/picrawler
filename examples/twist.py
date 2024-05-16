dd40de92c7a5a83bded0d08db20d66d2514996a3
Cavon Lee <cavonxx@gmail.com>
2024-05-16 10:22:12 +0800

    fix speed

--- examples/twist.py
+++ examples/twist.py
@@ -10,7 +10,7 @@ crawler = Picrawler()
 def twist(speed):
     new_step=[[50, 50, -80], [50, 50, -80],[50, 50, -80], [50, 50, -80]]
     for i in range(4):
-        for inc in range(30, 50, 5): 
+        for inc in range(30, 60, 5): 
             rise = [50,50,(-80+inc*0.5)]
             drop = [50,50,(-80-inc)]
 
@@ -18,6 +18,7 @@ def twist(speed):
             new_step[(i+2)%4] = drop
             new_step[(i+1)%4] = rise
             new_step[(i-1)%4] = drop
+            print(new_step)
             crawler.do_step(new_step,speed)
 
 
@@ -27,7 +28,7 @@ def main():
     music.music_set_volume(20)
 
     while True:
-        twist(speed=30) 
+        twist(speed=90) 
             
  
 if __name__ == "__main__":
