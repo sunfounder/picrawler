63971874815e9cf3153b1935dd468444efb2be36
haru$ <1736312423@qq.com>
2024-08-16 12:00:02 +0800

    add purchase link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,5 +100,12 @@ rst_epilog = """
 
     <a href="https://docs.sunfounder.com/projects/robot-hat-v4/en/latest/hardware_introduction.html" target="_blank">SunFounder Robot HAT</a>
 
+.. |link_Pi_Crawler| raw:: html
+
+    <a href="https://www.sunfounder.com/products/picrawler-robot-kit?_pos=1&_sid=2ebeb7ad0&_ss=r" target="_blank">Purchase Link for PiCrawler</a>
+
+.. |link_PiCrawler| raw:: html
+
+    <a href="https://www.sunfounder.com/products/picrawler-robot-kit?_pos=1&_sid=2ebeb7ad0&_ss=r" target="_blank">PiCrawler</a>
 
 """
